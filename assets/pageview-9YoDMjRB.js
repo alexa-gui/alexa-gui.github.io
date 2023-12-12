a24e97515ceab3925a8222e70ba4848cc286678b
alexa-gui <147514745+alexa-gui@users.noreply.github.com>
2023-12-12 06:57:02 +0000

    Deploying to gh-pages from @ alexa-gui/alexa-gui.github.io@27d8fb11d4c7b532003914463ba133acfc94d770 🚀

---
+++ assets/pageview-9YoDMjRB.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./app-HTBxy2WW.js";var a={provider:"Waline",dark:"auto",serverURL:"https://comment.axagiii.com",reaction:["/reactions/smile.png","/reactions/lovingit.png","/reactions/surprised.png","/reactions/awkward.png","/reactions/tears.png","/reactions/scared.png"],comment:!0,pageview:!0,copyright:!1};const n=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-HTBxy2WW.js").then(r=>r.L),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{n as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
