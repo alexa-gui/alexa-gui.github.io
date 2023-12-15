a257e4e77b668336b574c881e88e70ba7fd61f87
alexa-gui <zg319@cam.ac.uk>
2023-12-15 13:59:05 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,4 @@
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
@@ -27,6 +28,29 @@ export default defineUserConfig({
     ],],
 
   theme,
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
+  
+  markdown: {
+    headers: {
+      level: [2, 3, 4, 5, 6],
+    }
+  }
 
   // Enable it with pwa
   // shouldPrefetch: false,
---
+++ src/zh/blog1.md
@@ -0,0 +1,28 @@
+---
+title: 博客
+date: 2023-12-13 13:40:10
+author: Alexa
+isOriginal: true
+category: 
+    - Web design
+tag:
+    - web
+    - typora
+icon: coding
+sticky: false
+star: false
+article: true
+timeline: true
+image: false
+navbar: true
+sidebarIcon: true
+headerDepth: 5
+comment: true
+lastUpdated: true
+---
+
+
+
+- 文件名不要使用中文
+- 用减号代替空格
+- 文章内开始第一行要打三个- - -
